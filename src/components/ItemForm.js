6615d9646a6717850f078a0ce22100a9984d8afc
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-04-13 03:45:07 +0300

    Completed assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,5 +1,25 @@
 import React, { useState } from "react";
+function ItemForm({ onAddItem }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then((r) => r.json())
+      .then((newItem) => onAddItem(newItem));
+  }
 function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
